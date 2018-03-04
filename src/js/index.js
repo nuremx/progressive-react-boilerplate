221db73439a256b6cb1d856506b32dee4c772367
César Guadarrama Cantú <cesarguadarrama@icloud.com>
2018-03-04 14:54:58 -0600

    Added service worker registration

--- src/js/index.js
+++ src/js/index.js
@@ -10,3 +10,16 @@ ReactDOM.render(
   <Routes />,
   document.getElementById('root')
 )
+
+// Progressive Web App service worker registration
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/dist/sw.js')
+    .then(registration => {
+      console.info('SW registered: ', registration)
+    })
+    .catch(registrationError => {
+      console.info('SW registration failed: ', registrationError)
+    })
+  })
+}
