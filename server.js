6576118e35a9c455c3ff8d499222456650449e9c
César Guadarrama <cesarguadarrama@icloud.com>
2018-08-10 12:41:23 -0500

    General refactor and .env support

--- server.js
+++ server.js
@@ -2,39 +2,39 @@
 const express = require('express')
 const path = require('path')
 const helmet = require('helmet') // Basic headers protection
+const dotenv = require('dotenv')
 
 const bodyParser = require('body-parser')
-const winston = require('winston') // Logger
 const hpp = require('hpp')
+
+const mode = process.env.NODE_ENV
+const isProduction = mode === 'production'
+
+dotenv.config({
+  path: path.resolve(
+    `config/.env${isProduction ? '.production' : '.development'}`
+  ),
+})
+
+const { PORT = 8080, SERVER_ONLY = false } = process.env
+
 const app = express()
 
-const { PORT = 8080, NODE_ENV: mode } = process.env
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
 app.use(helmet())
 app.use(hpp())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-// Images and other static assets
-app.use('/static', express.static(path.resolve('static')))
-
-/*
-  You may want to add an API here
-  app.use('/v1', routerV1)
-*/
-
-// File bundles and processed assets
-app.use('/', express.static('dist'))
-
-// Check if we're in development mode to use webpackDevServer middleware
-if (mode === 'development') {
-  app.use(require(path.resolve('config/webpackDevServer')))
+if (!isProduction && !SERVER_ONLY) app.use(require(path.resolve('config/webpackDevServer')))
+if (isProduction && !SERVER_ONLY) {
+  app.use(express.static(path.resolve('dist')))
+  app.get('*', (req, res) => res.sendFile(path.resolve('dist/index.html')))
 }
-
-// Send index to all other routes (production)
-app.get('*', (req, res) => res.sendFile(path.resolve('dist/index.html')))
 
 // Start server
 app.listen(PORT, () =>
-  winston.info(`React Boilerplate server is listening on port: ${PORT}!`)
+  console.info(`React boilerplate is now running\n
+    Port: \t\t${PORT}
+    Server only: \t${SERVER_ONLY}
+    Mode: \t\t${mode}`)
 )
