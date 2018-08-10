a8c8e138212d45ff2e3273eb0df4a8e7cbf3b65c
César Guadarrama <cesarguadarrama@icloud.com>
2018-08-10 12:41:42 -0500

    Improved SW registration

--- src/index.js
+++ src/index.js
@@ -4,20 +4,34 @@ import ReactDOM from 'react-dom'
 import Routes from 'router'
 
 import './global.pcss'
-
+/* eslint-disable */
 const devMode = process.env.NODE_ENV === 'development'
+/* eslint-enable */
 
+// Service worker
 if (!devMode && 'serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
       .register('/sw.js')
       .then(registration => {
-        console.log('SW registered: ', registration)
+        registration.onupdatefound = () => {
+          const installingWorker = registration.installing
+          installingWorker.onstatechange = () => {
+            if (installingWorker.state === 'installed') {
+              if (navigator.serviceWorker.controller) {
+                console.warn('New content is available; please refresh') // eslint-disable-line no-console
+              } else {
+                console.info('Content is cached for offline use', registration)
+              }
+            }
+          }
+        }
       })
       .catch(registrationError => {
-        console.log('SW registration failed: ', registrationError)
+        console.info('SW registration failed: ', registrationError)
       })
   })
 }
 
+// Main render
 ReactDOM.render(<Routes />, document.getElementById('root'))
