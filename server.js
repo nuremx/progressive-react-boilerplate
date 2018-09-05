93e9442675f7184b3c4067124299ddfea30fa4a5
César Guadarrama <cesarguadarrama@icloud.com>
2018-09-04 20:13:32 -0500

    Refactor

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 /* eslint-env node */
-const express = require('express')
 const path = require('path')
+const express = require('express')
 const helmet = require('helmet') // Basic headers protection
 const dotenv = require('dotenv')
 
@@ -8,12 +8,10 @@ const bodyParser = require('body-parser')
 const hpp = require('hpp')
 
 const mode = process.env.NODE_ENV
-const isProduction = mode === 'production'
+const isProd = mode === 'production'
 
 dotenv.config({
-  path: path.resolve(
-    `config/.env${isProduction ? '.production' : '.development'}`
-  ),
+  path: path.resolve(`config/.env${isProd ? '.production' : '.development'}`),
 })
 
 const { PORT = 8080, SERVER_ONLY = false } = process.env
@@ -25,8 +23,8 @@ app.use(hpp())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-if (!isProduction && !SERVER_ONLY) app.use(require(path.resolve('config/webpackDevServer')))
-if (isProduction && !SERVER_ONLY) {
+if (!isProd && !SERVER_ONLY) app.use(require(path.resolve('config/webpackDevServer')))
+if (isProd && !SERVER_ONLY) {
   app.use(express.static(path.resolve('dist')))
   app.get('*', (req, res) => res.sendFile(path.resolve('dist/index.html')))
 }
